45f93805176d1f7e7061ae0bad2a504d6798eb9a
strawtype <>
2025-10-06 20:47:09 -0700

    --report HOURS :   will report entities with a last report values older than HOURS

--- query_panels.sh
+++ query_panels.sh
@@ -10,12 +10,12 @@ INFLUXDB_HOST="localhost:8086"
      ENTITIES="${DATA_DIR}/entities.txt"
     GRAPH_OUT="${DATA_DIR}/graph.json"
    PANELS_OUT="${DATA_DIR}/panels.json"
-  PANEL_COUNT=30  #ignored after --discover
+   REPORT_OUT="${DATA_DIR}/report.txt"
 ####Config end###
 
 usage () {
   echo "First time setup run:  $0 --discover"
-  echo ""
+  echo "Reports:               $0 --report HH"
   echo "Usage: $0 -d YYYY-MM-DD -h HH -e ENTITY -p POLL"
   echo
   echo "Options:"
@@ -46,8 +46,6 @@ queryflux () {
   fi
 }
 
-
-
 discover() {
   echo "Discovering lifetime_power and matching power sensors..."
 
@@ -63,7 +61,7 @@ discover() {
     echo ""
   fi
 
-  > "${DATA_DIR}/entities.txt"
+  > "${ENTITIES}"
   for lifetime_entity in "${lifetime_entities[@]}"; do
     if [[ "$lifetime_entity" =~ (c_|pv_)(lifetime_)?power$ ]]; then #skip consumption and virtual.  thanks u/babgvant!
       continue
@@ -76,16 +74,94 @@ discover() {
 
     if echo "$result" | jq -e '.results[0].series[0].values and (.results[0].series[0].values | length > 0)' > /dev/null 2>&1; then
       #echo "Found $power_entity matched from $lifetime_entity"
-      echo "- $power_entity" >> "${DATA_DIR}/entities.txt"
+      echo "- $power_entity" >> "${ENTITIES}"
       echo "        - $power_entity"
     fi
   done
-  
+
   echo ""
   echo "Use above in configuration.yaml for the timelapse_power_panels json_attributes"
   echo "Saved at $ENTITIES"
 }
 
+report() {
+  local nostats last report results notify
+  local now=$(date +%s)
+  local start=$((now - (HOUR * 3600)))
+  MODE="ENERGY"
+  UNITS="kWh"
+  H_START=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+  D_START=$(date -u -d "@$start" +"%Y-%m-%dT%H:%M:%SZ")
+
+  if [[ -s "$ENTITIES" ]]; then
+   nostats=$(writepanels | jq -r 'to_entries[] | select(.value == 0 or .value == null) | .key')
+
+
+  else
+    echo "No entities found.  First time setup run:  $0 --discover"
+    echo ""
+    exit 0
+  fi
+
+  for stat in $nostats; do
+
+    QUERY="SELECT MAX(value) FROM autogen.${UNITS} WHERE entity_id = '${stat}'"
+    last=$(queryflux)
+    utc=$(echo "$last" | jq -r '.results[0].series[0].values[0][0] // empty' | sed -E 's/\..*Z$//' | tr 'T' ' ')
+    epoch=$(date -u -D "%Y-%m-%d %H:%M:%S" -d "$utc" +%s)
+    local_t=$(date -d "@$epoch" "+%Y-%m-%d %H:%M:%S")
+    value=$(echo "$last" | jq -r '.results[0].series[0].values[0][1] // empty')
+    #results+="$stat | Last Update: UTC - $utc , Local - $local_t | Value: $value"$'\n'
+    results+="$stat | Last Update: $local_t | Value: $value"$'\n'
+
+  done
+
+  report="$results"
+  if [[ -n "$report" ]]; then
+       notify="WARN: missing over $HOUR hours of energy statistics from one or more devices"$'\n'
+       report="$notify$report"
+  fi
+  notify=""
+  QUERY="SELECT friendly_name_str,LAST(value) FROM s WHERE friendly_name_str =~ /PVS/ GROUP BY friendly_name_str"
+  results=$(queryflux)
+
+
+  #echo "$results" | jq -c '.results[0].series[]?' | while read -r i; do
+  while read -r i; do
+    last=$(echo "$i" | jq -r '.values[0][0]')
+    utc=$(echo "$last" | sed -E 's/\..*Z$//' | tr 'T' ' ')
+    epoch=$(date -u -D "%Y-%m-%d %H:%M:%S" -d "$utc" +%s)
+    local_t=$(date -d "@$epoch" "+%Y-%m-%d %H:%M:%S")
+    age=$((now - epoch))
+    if (( age >= 3600 )); then
+      notify="CRIT: PVS last update @ $local_t "
+    fi
+    name=$(echo "$i" | jq -r '.values[0][1]')
+    pvs=$(echo "$name" | grep -oE 'PVS[0-9]+')
+    serial=$(echo "$name" | grep -oE '[A-Z0-9]{16,}' | tail -n1 | grep -oE '.{5}$')
+    name="$pvs-$serial"
+
+    uptime=$(echo "$i" | jq -r '.values[0][2]')
+    if [[ uptime -le 600 ]]; then
+      notify='INFO: PVS Recent Reboot'
+    fi
+
+    if [[ -n "$epoch" && "$uptime" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+      reboot_epoch=$((epoch - ${uptime%.*}))
+      reboot_local=$(date -d "@$reboot_epoch" "+%Y-%m-%d %H:%M:%S")
+      reboot_utc=$(date -u -d "@$reboot_epoch" "+%Y-%m-%d %H:%M:%S")
+    else
+      reboot_time="Invalid"
+    fi
+    #report+="PVS: $name | Last Update: UTC - $utc , Local - $local_t  | Last Reboot: UTC - $reboot_utc , Local - $reboot_local | Uptime: $uptime sec"$'\n'
+  done  < <(echo "$results" | jq -c '.results[0].series[]?')
+
+  if [[ -n "$report" || -n "$notify" ]]; then
+    echo "$notify"
+    report+="$name | Last Update: $local_t  | Last Reboot: $reboot_local | Uptime: $uptime sec"$'\n'
+    echo "$report"
+  fi
+}
 
 writegraph () {
   local entity
@@ -99,6 +175,7 @@ writegraph () {
       entity="power"  ###NO MATCH, OVERRIDE THE MAIN POWER ENTITY HERE.
     fi
   fi
+
   if [[ "$MODE" == "ENERGY" || "$MODE" == "LIVE" ]]; then
     if [[ "$entity" == *_power ]]; then
       entity="${entity/_power/_lifetime_power}"
@@ -138,13 +215,6 @@ writepanels () {
          fi
       done
     fi
-  else
-    # the old way
-    local count=$((PANEL_COUNT + 2))
-    entities+=("${EN}power")
-    for (( i=3; i<=count; i++ )); do
-      entities+=("${EN}power_$i")
-    done
   fi
   local or_entities=""
   for entity in "${entities[@]}"; do
@@ -158,6 +228,13 @@ writepanels () {
   else                                  #power
     QUERY="SELECT FIRST(value) FROM autogen.${UNITS} WHERE (${or_entities}) AND time >= '${H_START}' and time <= '${H_END}'  GROUP BY entity_id"
   fi
+  if [[ "${FUNCNAME[1]}" == "report" ]]; then
+      local results merge_results
+      results=$(queryflux | jq -s -r '.[0].results[0].series[]? | { (.tags.entity_id): (.values[0][1] // 0 | tonumber) }' | jq -s add)
+      merge_results=$(jq -n --argjson polled "$results" --argjson listed "$(printf '%s\n' "${entities[@]}" | jq -R . | jq -s 'reduce .[] as $e ({}; .[$e] = null)')" '$listed + $polled')
+      echo "$merge_results"
+      return
+  fi
   #strip out lifetime_, fill with 0 when missing, write
   queryflux | jq -s -r '.[0].results[0].series[]? | { ((.tags.entity_id | sub("lifetime_"; ""))): (.values[0][1] // 0 | tonumber ) }' | jq -s add > "$PANELS_OUT"
 }
@@ -166,6 +243,15 @@ mkdir -p $DATA_DIR
 
 if [[ "$1" == "--discover" ]]; then
   discover
+  exit 0
+fi
+
+if [[ "$1" == "--report" ]]; then
+  if [ -n "$2" ]; then
+    HOUR="$2"
+  fi
+  : "${HOUR:=24}"  #default to 24
+  report
   exit 0
 fi
 
